5c252d451b2b8f10acfa78aadab36fef75bc6e9c
Fedor Pavlov <79637474+StrayDeR28@users.noreply.github.com>
2023-11-26 16:22:55 +0700

    Update server.cpp

--- OS_lab2/server.cpp
+++ OS_lab2/server.cpp
@@ -15,14 +15,12 @@ void sigHupHandler(int r)
 }
 void setupSignalHandling(sigset_t* origMask) 
 {
-	// Обработка сигнала SIGHUP
 	struct sigaction sa;
 	sigaction(SIGHUP, NULL, &sa);
 	sa.sa_handler = sigHupHandler;
 	sa.sa_flags |= SA_RESTART;
 	sigaction(SIGHUP, &sa, NULL);
 	
-	// Блокировка сигнала
 	sigset_t blockedMask;
 	sigemptyset(&blockedMask);
 	sigaddset(&blockedMask, SIGHUP);
@@ -30,7 +28,6 @@ void setupSignalHandling(sigset_t* origMask)
 }
 int main() 
 {
-    // Создание сокета
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) 
     {
@@ -38,37 +35,32 @@ int main()
         return 1;
     }
 
-    // Установка адреса сервера
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    // Привязка сокета к адресу и порту
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) 
     {
         std::cerr << "Bind failed" << std::endl;
         return 1;
     }
 
-    // Прослушивание входящих соединений
     if (listen(server_fd, 3) < 0) 
     {
         std::cerr << "Listen failed" << std::endl;
         return 1;
     }
-    
-    sigset_t origMask;
-	setupSignalHandling(&origMask);
 	
-    // Основной цикл обработки соединений
+    sigset_t origMask;
+    setupSignalHandling(&origMask);
+	
     while (true) 
     {
         fd_set read_fds;
         FD_ZERO(&read_fds);
         FD_SET(server_fd, &read_fds);
 
-        // Выбор активных сокетов с использованием pselect()
         int activity = pselect(server_fd + 1, &read_fds, NULL, NULL, NULL, &origMask);
         if ((activity < 0) && (errno != EINTR))
 	{
@@ -80,7 +72,7 @@ int main()
 	    std::cout << "Received SIGHUP signal" << std::endl;
 	    wasSigHup = 0;
 	}
-        // Если есть активность на серверном сокете, принимаем новое соединение
+	    
         if (FD_ISSET(server_fd, &read_fds)) 
 	{
             int new_socket = accept(server_fd, NULL, NULL);
