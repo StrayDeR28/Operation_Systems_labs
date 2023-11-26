b414bcd0f9d558e45a1c8000f751da7089313ef4
Fedor Pavlov <79637474+StrayDeR28@users.noreply.github.com>
2023-11-26 16:57:32 +0700

    a little cleaning

--- OS_lab2/server.cpp
+++ OS_lab2/server.cpp
@@ -9,6 +9,7 @@
 
 #define PORT 7777
 volatile sig_atomic_t wasSigHup = 0;
+
 void sigHupHandler(int r)
 {
 	wasSigHup = 1;
@@ -31,24 +32,24 @@ int main()
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) 
     {
-        std::cerr << "Error creating socket" << std::endl;
+        std::cout << "Error creating socket" << std::endl;
         return 1;
     }
-
+    
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-
+    
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) 
     {
-        std::cerr << "Bind failed" << std::endl;
+        std::cout << "Bind failed" << std::endl;
         return 1;
     }
-
+    
     if (listen(server_fd, 3) < 0) 
     {
-        std::cerr << "Listen failed" << std::endl;
+        std::cout << "Listen failed" << std::endl;
         return 1;
     }
 	
@@ -60,33 +61,33 @@ int main()
         fd_set read_fds;
         FD_ZERO(&read_fds);
         FD_SET(server_fd, &read_fds);
-
+        
         int activity = pselect(server_fd + 1, &read_fds, NULL, NULL, NULL, &origMask);
         if ((activity < 0) && (errno != EINTR))
-	{
-            std::cerr << "Error in pselect" << std::endl;
+        {
+            std::cout << "Error in pselect" << std::endl;
             return 1;
         }
         if (wasSigHup)
         {
-	    std::cout << "Received SIGHUP signal" << std::endl;
-	    wasSigHup = 0;
-	}
-	    
+            std::cout << "Received SIGHUP signal" << std::endl;
+            wasSigHup = 0;
+        }
         if (FD_ISSET(server_fd, &read_fds)) 
-	{
+        {
             int new_socket = accept(server_fd, NULL, NULL);
             if (new_socket < 0)
-	    {
-                std::cerr << "Accept failed" << std::endl;
+            {
+                std::cout << "Accept failed" << std::endl;
                 return 1;
             }
+            
             std::cout << "New connection accepted" << std::endl;
-
+            
             char buffer[1024] = {0};
             int valread = read(new_socket, buffer, 1024);
             if (valread > 0) 
-	    {
+            {
                 std::cout << "Message received: " << buffer << std::endl;
             }
             close(new_socket);
