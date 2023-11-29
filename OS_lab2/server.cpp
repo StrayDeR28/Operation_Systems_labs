283877afc41c3d45cdc720b3d04c9901da5f656f
Fedor Pavlov <79637474+StrayDeR28@users.noreply.github.com>
2023-11-29 14:19:07 +0700

    Update server.cpp

--- OS_lab2/server.cpp
+++ OS_lab2/server.cpp
@@ -7,7 +7,7 @@
 #include <sys/time.h>
 #include <arpa/inet.h>
 
-#define PORT 7777
+#define PORT 9999
 volatile sig_atomic_t wasSigHup = 0;
 
 void sigHupHandler(int r)
@@ -55,14 +55,40 @@ int main()
 	
     sigset_t origMask;
     setupSignalHandling(&origMask);
+    
+    int client_fd=0, max_fd;
 	
     while (true) 
     {
         fd_set read_fds;
         FD_ZERO(&read_fds);
-        FD_SET(server_fd, &read_fds);
         
-        int activity = pselect(server_fd + 1, &read_fds, NULL, NULL, NULL, &origMask);
+        if (max_fd < server_fd) max_fd = server_fd + 1; 
+		if (client_fd!=0)
+		{
+			FD_SET (client_fd, &read_fds);
+		    if (max_fd < client_fd) {max_fd = client_fd + 1;}
+		    if (FD_ISSET(client_fd, &read_fds))
+		    {
+				char buffer[1024] = {0};
+				int valread = read(client_fd, buffer, 1024);
+				if (valread > 0) 
+				{
+					std::cout << "Message received: " << buffer << std::endl;
+				}
+				if (valread == 0)
+				{
+					std::cout << "No message received " << std::endl;
+				}
+			}
+			close (client_fd);
+			FD_CLR(client_fd, &read_fds);
+			client_fd = 0;
+			continue;
+		}
+		
+		FD_SET(server_fd, &read_fds);
+        int activity = pselect(max_fd, &read_fds, NULL, NULL, NULL, &origMask);
         if ((activity < 0) && (errno != EINTR))
         {
             std::cout << "Error in pselect" << std::endl;
@@ -72,26 +98,23 @@ int main()
         {
             std::cout << "Received SIGHUP signal" << std::endl;
             wasSigHup = 0;
+            continue;
         }
+        
         if (FD_ISSET(server_fd, &read_fds)) 
         {
-            int new_socket = accept(server_fd, NULL, NULL);
-            if (new_socket < 0)
+			struct sockaddr_in client_fd_addr;
+			socklen_t client_fd_address_len = sizeof(client_fd_addr);
+			client_fd = accept(server_fd, (struct sockaddr*) &client_fd_addr, &client_fd_address_len);
+            if (client_fd < 0)
             {
                 std::cout << "Accept failed" << std::endl;
                 return 1;
             }
-            
             std::cout << "New connection accepted" << std::endl;
-            
-            char buffer[1024] = {0};
-            int valread = read(new_socket, buffer, 1024);
-            if (valread > 0) 
-            {
-                std::cout << "Message received: " << buffer << std::endl;
-            }
-            close(new_socket);
-        }
+		}
     }
+    close (client_fd);
+    close (server_fd);
     return 0;
 }
