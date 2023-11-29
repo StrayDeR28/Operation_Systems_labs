3777593bb783c429b6f3ab1f576da025fbcc99d7
Fedor Pavlov <79637474+StrayDeR28@users.noreply.github.com>
2023-11-29 14:19:47 +0700

    Update client.cpp

--- OS_lab2/client.cpp
+++ OS_lab2/client.cpp
@@ -4,9 +4,9 @@
 #include <unistd.h>
 #include <cstring>
 
-#define PORT 7777
-
-int main() {
+#define PORT 9999
+int main() 
+{
     int clientSocket;
     struct sockaddr_in serverAddr;
 
@@ -16,23 +16,23 @@ int main() {
         std::cout << "Error creating socket" << std::endl;
         return 1;
     }
-
+    
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(PORT);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
-
+    
     if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) 
     {
         std::cout << "Error connecting to server" << std::endl;
         return 1;
     }
-
+    
     std::cout << "Enter message for server: ";
     std::string message;
     std::getline(std::cin, message);
     send(clientSocket, message.c_str(), strlen(message.c_str()), 0);
-
+    
     close(clientSocket);
-
+    
     return 0;
 }
